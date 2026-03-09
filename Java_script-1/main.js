c7b358c4ed1e1f0063f66f5f47741c3b34f78459
Ilushka-b1t <ilak29866@gmail.com>
2026-03-09 17:24:53 +0500

    Homework_JS-1

---
+++ Java_script-1/main.js
@@ -0,0 +1,26 @@
+// Задача 1.//
+const firstName = "Ivan";
+const lastName = "Ivanov";
+const isStudent = true;
+
+
+// Задача 2.//
+const age = 20;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+console.log(birthYear)
+
+
+// Задача 3.//
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`);
+
+
+// Задача 4.//
+
+let a = '123';
+let b = +'456';
+let c = Number('789');
+let d = Boolean(0);
+let e = Boolean(' ');
+let result = a + b + c + d + e;
+console.log(result); //123456789falsetrue//
